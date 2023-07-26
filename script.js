5a30ebc5eead9dab8bda47555652ce24075abe2d
Jennifer Lopez <jenniferlopezh0@gmail.com>
2023-07-25 22:23:18 -0700

    added countdown

--- script.js
+++ script.js
@@ -13,8 +13,38 @@ function copyURLToClipboard() {
     const customAlertOverlay = document.getElementById("customAlertOverlay");
     customAlertOverlay.style.display = "flex";
 
-    // Hide the custom alert after 2 seconds (optional)
+    // Hide the custom alert after 2 seconds
     setTimeout(function () {
         customAlertOverlay.style.display = "none";
     }, 1000);
+}
+
+
+// This is for the countdown portion
+const getCountdownParagraph = document.getElementById("countdownOutput");
+
+window.addEventListener("load", countdown);
+
+function countdown() {
+    const eventDate = new Date('July 28, 2023 18:00:00');
+    const today = new Date();
+
+    const timeLeft = eventDate.getTime() - today.getTime();
+
+    const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    if (daysLeft <= 0 && hoursLeft <= 0 && minutesLeft <= 0 && secondsLeft <= 0) {
+        document.getElementById("countdownTitle").innerText = "Registration has closed";
+        getCountdownParagraph.innerText = "0d 0h 0m 0s";
+        document.getElementById("regBtnDiv").style.display = "none";
+        return;
+    }
+    let str = daysLeft + "d " + hoursLeft + "h "
+        + minutesLeft + "m " + secondsLeft + "s ";
+    getCountdownParagraph.innerText = str;
+
+    setTimeout(countdown, 1000);
 }
